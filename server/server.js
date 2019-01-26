87e7668ace5792b7fd950568c44e1c815fdefdcd
Hemin Johnson <achayan1992@gmail.com>
2019-01-26 21:49:21 +0100

    feat: configure socket with server

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@
 const path = require('path')
 const express = require('express')
 const http = require('http')
+var socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 var app = express()
 var server = http.createServer(app)
+var io = socketIO(server)
 
 app.use(express.static(publicPath))
 

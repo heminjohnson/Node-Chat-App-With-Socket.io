d388dd64f6ca942bdab3984be09f2d930ca9c604
Hemin Johnson <achayan1992@gmail.com>
2019-01-26 21:26:54 +0100

    feat: use http server instead of express server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-console */
 const path = require('path')
 const express = require('express')
+const http = require('http')
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 var app = express()
+var server = http.createServer(app)
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Server is up on ${port}`)
 })

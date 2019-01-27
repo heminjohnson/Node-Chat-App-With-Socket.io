934f6a6dd0d69d71ab062496dd9df53108c302fd
Hemin Johnson <achayan1992@gmail.com>
2019-01-27 15:12:17 +0100

    feat: add new message and create message events

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,17 @@ var socket = io()
 
 socket.on('connect', function() {
 	console.log('connected to server')
+  
+	socket.emit('createMessage', {
+		from: 'Hemin',
+		text: 'yup that works for me'
+	})
 })
 
 socket.on('disconnect', function() {
 	console.log('disconnected from server')
+})
+
+socket.on('newMessage', function(message) {
+	console.log('newMessage', message)
 })

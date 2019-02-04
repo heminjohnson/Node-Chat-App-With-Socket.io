fad8e40a51742fab1ac9593923132dcd44c474b0
Hemin Johnson <achayan1992@gmail.com>
2019-02-04 18:21:14 +0100

    feat: add jquery to update the dom element to show the connected users

--- public/js/index.js
+++ public/js/index.js
@@ -10,20 +10,16 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
 	console.log('newMessage', message)
-})
-
-socket.emit('createMessage', {
-	from: 'Frank',
-	text: 'Hi'
-}, function (data) {
-	console.log(`Got it, ${data}`)
+	var li = jQuery('<li></li>')
+	li.text(`${message.from}: ${message.text}`)
+	jQuery('#messages').append(li)
 })
 
 jQuery('#message-form').on('submit', function (e) {
 	e.preventDefault()
 
 	socket.emit('createMessage', {
-		from: 'User'
+		from: 'User',
 		text: jQuery('[name=message]').val()
 	}, function () {
 		

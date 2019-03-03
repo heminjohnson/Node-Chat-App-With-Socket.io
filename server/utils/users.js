387f828278411a7a7c77e6fb7bd5dbe5cd851a89
Hemin Johnson <achayan1992@gmail.com>
2019-03-03 20:47:21 +0100

    feat: add class function for getting all the users in one room

--- server/utils/users.js
+++ server/utils/users.js
@@ -7,6 +7,12 @@ class Users {
 		this.users.push(user)
 		return user
 	}
+	getUserList(room) {
+		var users = this.users.filter((user) => user.room === room)
+		var namesArray = users.map((user) => user.name)
+
+		return namesArray
+	}
 }
 
 module.exports = {Users}

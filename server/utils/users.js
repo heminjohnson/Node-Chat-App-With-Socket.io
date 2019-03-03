c9175bba2a20c203b59a7701a5dbf8f2175fc97e
Hemin Johnson <achayan1992@gmail.com>
2019-03-03 21:10:16 +0100

    feat: add the remove user function against the test

--- server/utils/users.js
+++ server/utils/users.js
@@ -7,6 +7,13 @@ class Users {
 		this.users.push(user)
 		return user
 	}
+	removeUser (id) {
+		var user = this.getUser(id)
+		if(user) {
+			this.users = this.users.filter((user) => user.id !== id)
+		}
+		return user
+	}
 	getUser (id) {
 		return this.users.filter((user) => user.id === id)[0]
 	}
